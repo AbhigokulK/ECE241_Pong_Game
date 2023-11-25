240e817d56d0fc60bbe1498c69b1ce3a5a8efdf3
Abhigokul Kupendiran <138940977+AbhigokulK@users.noreply.github.com>
2023-11-25 16:53:51 -0500

    Update ball_vga_interface.v
    Added parameters for paddle info, 
    TO DO:
    implement render ordering for paddles and ball

--- ball_vga_interface.v
+++ ball_vga_interface.v
@@ -41,6 +41,25 @@ module fill
 	wire resetn;
 	assign resetn = KEY[0];
 	
+	parameter 	X_BOXSIZE = 8'd4,   // Box X dimension
+				Y_BOXSIZE = 7'd4,   // Box Y dimension
+				X_SCREEN_PIXELS = 'd320,  // X screen width for starting resolution and fake_fpga (was 9*)
+				Y_SCREEN_PIXELS = 'd240,  // Y screen height for starting resolution and fake_fpga (was 7*)
+				CLOCKS_PER_SECOND = 50000000, // 50 MHZ for fake_fpga (was 5KHz*)
+				X_MAX = X_SCREEN_PIXELS - 1 - X_BOXSIZE, // 0-based and account for box width
+				Y_MAX = Y_SCREEN_PIXELS - 1 - Y_BOXSIZE,
+				
+				PADDLE_X = 5,
+				PADDLE_Y = 100,
+				PADDLE_OFFSET = 10,
+				PADDLE_MAX_Y = Y_SCREEN_PIXELS - 1 - PADDLE_Y,
+
+				FRAME_RATE = 'd60,
+				PULSES_PER_SIXTIETH_SECOND = CLOCKS_PER_SECOND / 60,
+				RATE = 'd1,
+				MAX_RATE = 'd4,
+				PULSE_HOLD_TIME = 1;
+
 	// Create the colour, x, y and writeEn wires that are inputs to the controller.
 
 	wire [2:0] colour;
@@ -67,7 +86,7 @@ module fill
 			.VGA_BLANK(VGA_BLANK_N),
 			.VGA_SYNC(VGA_SYNC_N),
 			.VGA_CLK(VGA_CLK));
-		defparam VGA.RESOLUTION = "160x120";
+		defparam VGA.RESOLUTION = "320x240";
 		defparam VGA.MONOCHROME = "FALSE";
 		defparam VGA.BITS_PER_COLOUR_CHANNEL = 1;
 		defparam VGA.BACKGROUND_IMAGE = "black.mif";
@@ -76,43 +95,160 @@ module fill
 	// for the VGA controller, in addition to any other functionality your design may require.
 	
 	/*
-	Part 2 FPGA mapping:
-	iClock -> Clock_50
-	iResetn -> KEY[0]
-	iColour[2:0] -> SW[2:0]
-	
-	oColour[2:0] -> LEDR[9:7]
-	oNewFrame -> LEDR[0]
-	oPlot -> LEDR[1]
-	oX[9:0] -> HEX2, 1, 0
-	oY[8:0] -> HEX3, 4, 5
-	*/
+	FPGA MAPPING:
+		resetn (reset active low) -> KEY[0]
+		iColour -> SW[2:0]
+		iBlack (active high clear screen) -> !KEY[3]
+		iEnable -> SW[9]
+		LEDR[7:5] -> oColour
+		LEDR[9:8] -> {KEY[3], KEY[0]}
+		
+		LEDR[0] -> LHS Goal Hit (on for 1s)
+		LEDR[1] -> RHS Goal Hit (on for 1s)
+		LEDR[2] -> Boundary Hit (on for 1s)
+		
+		HEX0, HEX1, HEX2 -> ball_x_coordinate
+		HEX3, HEX4, HEX5 -> ball_y_coordinate
+	/*
+	module ball_movement:
+		input wire [2:0] 	iColour;
+		input wire 	    	iResetn;
+		input wire 	    	iClock;
+		input wire			iBlack;
+		input wire			iEnable;
 
-	//module part2(iResetn, iPlotBox, iBlack, iColour, iLoadX, iXY_Coord, iClock,
-	//					oX, oY, oColour, oPlot, oDone);
-	//***** CHECK PARAMETERS!!!
-	part1 #(.X_BOXSIZE('d4), .Y_BOXSIZE('d2), .X_SCREEN_PIXELS('d160), .Y_SCREEN_PIXELS('d120),
-				.FRAMES_PER_UPDATE('d10), .RATE('d1)) p1
-				(.iResetn(resetn), .iColour(SW[2:0]), .iClock(CLOCK_50),
-				.oX(x), .oY(y), .oColour(colour), .oPlot(writeEn), .oNewFrame(LEDR[0]));
-				
+		input[($clog2(PADDLE_MAX_Y)):0] lhs_paddle_y;
+		input[($clog2(PADDLE_MAX_Y)):0] rhs_paddle_y;
+
+		output wire [($clog2(X_SCREEN_PIXELS)):0] oX;         // VGA pixel coordinates
+		output wire [($clog2(Y_SCREEN_PIXELS)):0] oY;
+
+		output wire [2:0] 	oColour;     // VGA pixel colour (0-7)
+		output wire 	    oPlot;       // Pixel drawn enable
+		output wire [($clog2(X_MAX)):0] oBall_X;
+		output wire [($clog2(Y_MAX)):0] oBall_Y;
+		output wire lhs_score;
+		output wire rhs_score;
+		output wire boundaryHit;
+
+	parameter
+		X_BOXSIZE = 8'd4,   // Box X dimension
+		Y_BOXSIZE = 7'd4,   // Box Y dimension
+		X_SCREEN_PIXELS = 10'd320,  // X screen width for starting resolution and fake_fpga (was 9*)
+		Y_SCREEN_PIXELS = 9'd240,  // Y screen height for starting resolution and fake_fpga (was 7*)
+		CLOCKS_PER_SECOND = 50000000, // 50 MHZ for fake_fpga (was 5KHz*)
+		X_MAX = X_SCREEN_PIXELS - 1 - X_BOXSIZE, // 0-based and account for box width
+		Y_MAX = Y_SCREEN_PIXELS - 1 - Y_BOXSIZE,
+
+    	FRAMES_PER_UPDATE = 'd15,
+    	PULSES_PER_SIXTIETH_SECOND = CLOCKS_PER_SECOND / 60,
+		RATE = 'd1,
+		MAX_RATE = 'd5,
+		TIME_TILL_ACCEL = 'd2,
+		PADDLE_X = 'd4,
+		PADDLE_Y = 'd15,
+		PADDLE_OFFSET = 'd2,
+		PADDLE_MAX_Y = Y_SCREEN_PIXELS - 1 - PADDLE_Y;
+	*/
+	wire [($clog2(X_MAX)):0]ball_x_coord;
+	wire [($clog2(X_MAX)):0]ball_y_coord;
+
+	wire lhs_pulse, rhs_pulse, boundary_pulse;
+
+	// KEYS ARE ACTIVE LOW (ie low when pushed down)
+	ball_movement #(.X_BOXSIZE(X_BOXSIZE), .Y_BOXSIZE(Y_BOXSIZE), 
+					.X_SCREEN_PIXELS(X_SCREEN_PIXELS), .Y_SCREEN_PIXELS(Y_SCREEN_PIXELS),
+					.PADDLE_X(PADDLE_X), .PADDLE_Y(PADDLE_Y), 
+					.PADDLE_OFFSET(PADDLE_OFFSET), .PADDLE_MAX_Y(PADDLE_MAX_Y),
+					.FRAMES_PER_UPDATE(FRAME_RATE), .RATE(RATE), .MAX_RATE(MAX_RATE)) ball1
+					(.iResetn(resetn), .iColour(SW[2:0]), .iClock(CLOCK_50),
+					.iBlack(!KEY[3]), .iEnable(SW[9]), // IMPORTANT, SEE NOTE
+					.lhs_paddle_y(1), .rhs_paddle_y(15), // these are for debugging!!!
+					.oX(x), .oY(y), .oColour(colour), .oPlot(writeEn), .oBall_X(ball_x_coord), .oBall_Y(ball_y_coord),
+					.lhs_score(lhs_pulse), .rhs_score(rhs_pulse), .boundaryHit(boundary_pulse));
+					
 	
-	assign LEDR[1] = writeEn;
-	assign LEDR[4:2] = colour;
-	assign LEDR[9:6] = KEY;
+	assign LEDR[7:5] = colour;
+	assign LEDR[9:8] = {!KEY[3], !KEY[0]};
 	
+	holdPulse #(CLOCKS_PER_SECOND,
+				PULSE_HOLD_TIME)
+	lhs_LED(CLOCK_50,
+			lhs_pulse,
+			resetn,
+			LEDR[0]);
+
+	holdPulse #(CLOCKS_PER_SECOND,
+				PULSE_HOLD_TIME)
+			rhs_LED(CLOCK_50,
+					rhs_pulse,
+					resetn,
+					LEDR[1]);
+
+	holdPulse #(CLOCKS_PER_SECOND,
+				PULSE_HOLD_TIME)
+	boundary_LED(CLOCK_50,
+				boundary_pulse,
+				resetn,
+				LEDR[2]);
+
 	// Output coordinates
-	hex_decoder hex0(x[3:0], HEX0);
-	hex_decoder hex1(x[7:4], HEX1);
-	hex_decoder hex2({2'b00, x[9], x[8]}, HEX2);
+	hex_decoder hex0(ball_x_coord[3:0], HEX0);
+	hex_decoder hex1(ball_x_coord[7:4], HEX1);
+	hex_decoder hex2({2'b00, ball_x_coord[9:8]}, HEX2);
 	
-	hex_decoder hex3(y[3:0], HEX3);
-	hex_decoder hex4(y[7:4], HEX4);
-	hex_decoder hex5({3'b000, y[8]}, HEX5);
+	hex_decoder hex3(ball_y_coord[3:0], HEX3);
+	hex_decoder hex4(ball_y_coord[7:4], HEX4);
+	hex_decoder hex5({3'b000, ball_y_coord[8]}, HEX5);
 	
 endmodule
 
 
+module holdPulse
+#(
+	parameter 	CLOCK_FREQ = 50000000,
+					HOLD_TIME = 1
+
+)
+(
+	input clk, 
+	input pulse, 
+	input resetn, 
+	output heldPulse
+);
+	
+	reg[($clog2(HOLD_TIME*CLOCK_FREQ)):0] count;
+	reg hold;
+
+	localparam max_count = HOLD_TIME*CLOCK_FREQ;
+
+	always@(posedge clk) begin
+		if(!resetn) begin
+			// END COUNT IMMEDIATELY, and stop counting
+			count <= 0;
+			hold <= 0;
+		end
+		else begin
+			if(pulse) begin
+				// reset counter on pulse
+				count <= max_count;
+				hold <= 1;
+			end
+			if(count > 0) begin
+				// begin counting down! (should only occur AFTER pulse has been given)
+				count <= count - 1;
+				hold <= 1;
+			end
+			else begin
+				// counter ends!
+				hold <= 0;
+			end
+		end
+	end
+
+	assign heldPulse = hold;
+
+endmodule
 
 
 module hex_decoder(c, display);
